0f54619b0ac2fc54796a7a4c218753d8fcff5c76
Egor-kl <57603709+Egor-kl@users.noreply.github.com>
2021-11-13 16:50:36 +0300

    feat: add calculator

---
+++ src/calculator/main.rb
@@ -0,0 +1,37 @@
+def multiply(first_number, second_number)
+  return first_number * second_number
+end
+
+def division(first_number, second_number)
+  if second_number == 0
+    return 0
+  end
+  return first_number / second_number
+end
+
+def addition(first_number, second_number)
+  return first_number + second_number
+end
+
+def subtraction(first_number, second_number)
+  return first_number - second_number
+end
+
+puts 'First number:'
+first_number = gets.chomp.to_i
+puts 'Second number:'
+second_number = gets.chomp.to_i
+puts 'Operation:'
+operation = gets.chomp
+
+if operation == '*'
+  puts multiply(first_number, second_number)
+elsif operation == '/'
+  puts division(first_number, second_number)
+elsif operation == '+'
+  puts addition(first_number, second_number)
+elsif operation == '-'
+  puts subtraction(first_number, second_number)
+else
+  puts 'Are you idiot?'
+end
